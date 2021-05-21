2dc5845645200aa651be14b87881e2bd8064cb04
Cody_Jones <cbjones93@gmail.com>
2021-05-21 11:40:04 -0500

    intro

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+// Console.Write("Who would you like to say hello to? ");
+
+// string name = Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(name))
+// {
+//     Console.WriteLine("Fine, don't say 'hello'!");
+// }
+// else
+// {
+//     Console.WriteLine($"Hello, {name}!");
+// }
+
+
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"{lastName} {firstName}");
+
+string reverse =string.Empty;
+string fullName = firstName + " " + lastName;
+
+
+
+for (int num = 10; num >=1; num--)
+{
+    Console.WriteLine(num);
+}
